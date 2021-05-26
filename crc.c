99f867d3e0a78109ea75594ebe3127a5b5c49643
1NT18IS173 <83108404+1NT18IS173@users.noreply.github.com>
2021-05-26 21:47:10 +0530

    Add files via upload

---
+++ crc.c
@@ -0,0 +1,48 @@
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int crc(char *ip, char *op, char *poly, int mode)
+{
+    strcpy(op, ip);
+    if (mode) {
+        for (int i = 1; i < strlen(poly); i++)
+            strcat(op, "0");
+    }
+    for (int i = 0; i < strlen(ip); i++) {
+        if (op[i] == '1') {
+            for (int j = 0; j < strlen(poly); j++) {
+                if (op[i + j] == poly[j])
+                    op[i + j] = '0';
+                else
+                    op[i + j] = '1';
+            }
+        }
+    }
+    for (int i = 0; i < strlen(op); i++)
+        if (op[i] == '1')
+            return 0;
+    return 1;
+}
+
+int main()
+{
+    char ip[50], op[50], recv[50];
+    char poly[10];
+    cout << "Enter the input message in binary: ";
+    cin >> ip;
+    cout<<"enter the Polynomial: ";
+    cin>>poly;
+    crc(ip, op, poly, 1);
+    cout << "The transmitted message is: " << ip << op + strlen(ip) << endl;
+    cout << "Enter the recevied message in binary: ";
+    cin >> recv;
+    if (crc(recv, op, poly, 0))
+        cout << "No error in data";
+    else
+        cout << "Error in data transmission has occurred";
+
+    return 0;
+}
